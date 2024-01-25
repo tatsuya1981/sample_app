2b442279761fd8cedb1822083a0b1de2bab0f15b
tatsuya <clarkmaster@icloud.com>
2024-01-25 23:58:49 +0900

    Gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+  # GET \users \:id
   def show
     # @user = User.first;
     @user = User.find(params[:id]);
+    
+    # => app/views/users/show.html.erb
   end
 
+    # GET  /users/new
   def new
     # => app/views/users/new.html.erb
   end
