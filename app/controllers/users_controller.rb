b21a4e54d3c544be173fbcb341fb7e0559b32f88
tatsuya <clarkmaster@icloud.com>
2024-01-27 18:44:09 +0900

    sign_up_false

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,20 @@ class UsersController < ApplicationController
     # GET  /users/new or /signup
   def new
     @user = User.new
-    # => app/views/users/new.html.erb
   end
-end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #  => Success
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    # Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end 
