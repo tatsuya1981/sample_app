16c419e967afe1595a4e73df0e241e16b94c198e
tatsuya <clarkmaster@icloud.com>
2024-01-29 23:37:29 +0900

    flash

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+    # ✖︎ @session = Session.new
+    # ○ scope: :session + url: login_path
+  end
+
+  # POST /login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+end
